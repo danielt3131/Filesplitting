600849ab450f75866a086b30e7475bab804ad1f8
Daniel Thompson <danielt3131@gmail.com>
2023-12-14 09:02:13 -0500

    Added in a selection to either split the file or merge the file.

--- src/main/java/io/github/danielt3131/filesplitting/Main.java
+++ src/main/java/io/github/danielt3131/filesplitting/Main.java
@@ -1,6 +1,7 @@
 package io.github.danielt3131.filesplitting;
 
 import java.io.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,35 +13,42 @@ public class Main {
         long chunkSize = 1024 * 1024;  // 1 MiB
         String inputName = "./input";
         String outputName;
-        File inputFile = new File(inputName);
-        if (!inputFile.exists() || !inputFile.isFile()){
-            return;
-        }
-        byte buffer[] = new byte[(int) chunkSize];  // 1 MiB buffer
-        long numberOfChunks = inputFile.length() / chunkSize;
-        System.out.printf("Size : %d\n", inputFile.length() / chunkSize);
-        long remainderChunkSize = inputFile.length() % chunkSize;
-        try {
-            FileInputStream inputStream = new FileInputStream(inputFile);
-            long i;
-            for (i = 0; i < numberOfChunks; i++) {
-                inputStream.read(buffer, 0, buffer.length);
-                outputName = String.format("%s.%d", inputName, i);
-                FileOutputStream outputStream = new FileOutputStream(outputName);
-                outputStream.write(buffer, 0, buffer.length);
-                outputStream.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press 1 to split the file input or press 2 to merge the split files back together");
+        int selection =  scanner.nextInt();
+        if (selection == 1){
+            File inputFile = new File(inputName);
+            if (!inputFile.exists() || !inputFile.isFile()){
+                return;
             }
-            if (remainderChunkSize != 0){
-                inputStream.read(buffer, 0, (int) chunkSize);
-                outputName = String.format("%s.%d", inputName, i);
-                FileOutputStream outputStream = new FileOutputStream(outputName);
-                outputStream.write(buffer, 0, (int) remainderChunkSize);
-                outputStream.close();
+            byte buffer[] = new byte[(int) chunkSize];  // 1 MiB buffer
+            long numberOfChunks = inputFile.length() / chunkSize;
+            System.out.printf("Size : %d\n", inputFile.length() / chunkSize);
+            long remainderChunkSize = inputFile.length() % chunkSize;
+            try {
+                FileInputStream inputStream = new FileInputStream(inputFile);
+                long i;
+                for (i = 0; i < numberOfChunks; i++) {
+                    inputStream.read(buffer, 0, buffer.length);
+                    outputName = String.format("%s.%d", inputName, i);
+                    FileOutputStream outputStream = new FileOutputStream(outputName);
+                    outputStream.write(buffer, 0, buffer.length);
+                    outputStream.close();
+                }
+                if (remainderChunkSize != 0){
+                    inputStream.read(buffer, 0, (int) chunkSize);
+                    outputName = String.format("%s.%d", inputName, i);
+                    FileOutputStream outputStream = new FileOutputStream(outputName);
+                    outputStream.write(buffer, 0, (int) remainderChunkSize);
+                    outputStream.close();
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } else if (selection == 2){
+
         }
     }
 }
