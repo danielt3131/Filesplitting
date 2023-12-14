d8930653c4b09871563fbbb1de67a7e6f74692b3
Daniel Thompson <danielt3131@gmail.com>
2023-12-14 08:57:18 -0500

    Debugging calculating the number of chunks

--- src/main/java/io/github/danielt3131/filesplitting/Main.java
+++ src/main/java/io/github/danielt3131/filesplitting/Main.java
@@ -1,20 +1,46 @@
 package io.github.danielt3131.filesplitting;
 
+import java.io.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        int test = 2;
-        System.out.printf("Hello world %d\n", test);
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        long chunkSize = 101048576;
+        String inputName = "./input";
+        String outputName;
+        File inputFile = new File(inputName);
+        if (!inputFile.exists() || !inputFile.isFile()){
+            return;
+        }
+        byte buffer[] = new byte[(int) chunkSize];
+        long numberOfChunks = inputFile.length() / chunkSize;
+        System.out.printf("Size : %d\n", inputFile.length() / chunkSize);
+        long remainderChunkSize = inputFile.length() % chunkSize;
+        try {
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            long i;
+            for (i = 0; i < numberOfChunks; i++) {
+                inputStream.read(buffer, 0, buffer.length);
+                outputName = String.format("%s.%d", inputName, i);
+                FileOutputStream outputStream = new FileOutputStream(outputName);
+                outputStream.write(buffer, 0, buffer.length);
+                outputStream.close();
+            }
+            if (remainderChunkSize != 0){
+                inputStream.read(buffer, 0, (int) chunkSize);
+                outputName = String.format("%s.%d", inputName, i);
+                FileOutputStream outputStream = new FileOutputStream(outputName);
+                outputStream.write(buffer, 0, (int) remainderChunkSize);
+                outputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
